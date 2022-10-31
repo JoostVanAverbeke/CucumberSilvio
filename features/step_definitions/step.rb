e1c37406ffc2e38dda23737e0803ad4e7860aa64
Joost Van Averbeke <Joost.VanAverbeke@mips.be>
2022-10-31 16:16:42 +0100

    Added the Silvio Person URL feature
    Implemented the step definitions
    Added clearing the instance data in the database of the GLIMS application

--- features/step_definitions/step.rb
+++ features/step_definitions/step.rb
@@ -10,6 +10,34 @@ When("I send a valid URL message to the translator service") do
   @response_message = send_request(initiating_message)
 end
 
+When("I send a person URL message with an identification {string} assigned by {string}, to the translator service") do |string, string2|
+  create_person_type = FactoryGirl.build(:person_type, :create)
+  create_person_type.identificationList  = IdentificationListTypeBuilder.new.
+          add(FactoryGirl.build(:mipsn_identifications,
+                                source: FactoryGirl.build(:correspondent_type, internalId: string2),
+                                code: string)).build
+
+  initiating_message = FactoryGirl.build(:message, correspondent: create_person_type)
+  @response_message = send_request(initiating_message)
+end
+
+When("I send a person URL message with the following identifications to the translator service") do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  create_person_type = FactoryGirl.build(:person_type, :create)
+  builder = IdentificationListTypeBuilder.new
+  # table.hashes
+  # Converts the table to an array of hashes, where the keys are the table headers from the first row
+  #
+  table.hashes.each do |hash|
+    builder.add(FactoryGirl.build(:mipsn_identifications,
+                                source: FactoryGirl.build(:correspondent_type, internalId: hash[:assigned_by]),
+                                code: hash[:code]))
+  end
+  create_person_type.identificationList = builder.build
+  initiating_message = FactoryGirl.build(:message, correspondent: create_person_type)
+  @response_message = send_request(initiating_message)
+end
+
 Then("the translator service responds with an URL accept message") do
   expect(@response_message.correspondent.command).to eq_enumerator('Accept')
 end
